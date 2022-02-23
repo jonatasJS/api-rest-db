eb51cdce580d46de03c95f7970a814d153122d72
sozinho <soaresjonatas398@gmail.com>
2022-02-23 14:03:25 +0000

    add

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 
 const app = express();
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString);
+const database = mongoose.connection
 
 app.use(express.json());
+app.use('/api', routes);
+
+app.get('/', (req, res) => res.send('teste'))
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
 
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`)
 })
+
+// 
